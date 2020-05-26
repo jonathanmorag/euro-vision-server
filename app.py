383d66b340f1d6acdfeae1da0cd81221ee0ad6c1
Jonathan Morag <moragjonathan@gmail.com>
2020-05-26 23:39:47 +0300

    Added predict_match function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from model import Model
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,4 +9,14 @@ model.load_weights()
 
 @app.route("/")
 def index():
-    return model.predictSingleMatch('France', 'Spain')
+    return '<h1>Welcome to Euro-Vision Flask server!</h1>'
+
+
+@app.route("/predict_match", methods=['GET'])
+def predict_match():
+    try:
+        team1 = request.args.get("team1")
+        team2 = request.args.get("team2")
+        return model.predictSingleMatch(team1, team2)
+    except Exception as e:
+        return "Error has occured"
